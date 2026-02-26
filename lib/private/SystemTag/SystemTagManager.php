c483a4ed51fbddcbe9172bd9a19d65095f5dbac8
Carl Schwan <carlschwan@kde.org>
2026-02-26 23:27:14 +0100

    refactor: Add new modern events for system tag lifecycle
    The old one was deprecated in 22.0.0 but didn't had a replacement.
    
    Signed-off-by: Carl Schwan <carlschwan@kde.org>

--- lib/private/SystemTag/SystemTagManager.php
+++ lib/private/SystemTag/SystemTagManager.php
@@ -16,6 +16,9 @@ use OCP\IDBConnection;
 use OCP\IGroupManager;
 use OCP\IUser;
 use OCP\IUserSession;
+use OCP\SystemTag\Events\TagCreatedEvent;
+use OCP\SystemTag\Events\TagDeletedEvent;
+use OCP\SystemTag\Events\TagUpdatedEvent;
 use OCP\SystemTag\ISystemTag;
 use OCP\SystemTag\ISystemTagManager;
 use OCP\SystemTag\ManagerEvent;
@@ -209,6 +212,7 @@ class SystemTagManager implements ISystemTagManager {
 		$this->dispatcher->dispatch(ManagerEvent::EVENT_CREATE, new ManagerEvent(
 			ManagerEvent::EVENT_CREATE, $tag
 		));
+		$this->dispatcher->dispatchTyped(new TagCreatedEvent($tag));
 
 		return $tag;
 	}
@@ -289,6 +293,7 @@ class SystemTagManager implements ISystemTagManager {
 		$this->dispatcher->dispatch(ManagerEvent::EVENT_UPDATE, new ManagerEvent(
 			ManagerEvent::EVENT_UPDATE, $afterUpdate, $beforeUpdate
 		));
+		$this->dispatcher->dispatchTyped(new TagUpdatedEvent($afterUpdate, $beforeUpdate));
 	}
 
 	public function deleteTags($tagIds): void {
@@ -331,6 +336,7 @@ class SystemTagManager implements ISystemTagManager {
 			$this->dispatcher->dispatch(ManagerEvent::EVENT_DELETE, new ManagerEvent(
 				ManagerEvent::EVENT_DELETE, $tag
 			));
+			$this->dispatcher->dispatchTyped(new TagDeletedEvent($tag));
 		}
 
 		if ($tagNotFoundException !== null) {
