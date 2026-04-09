d8b50c6ee02630b8d6eb79de60c8e25230bb2a2c
Carl Schwan <carlschwan@kde.org>
2026-04-09 11:01:01 +0200

    refactor(hooks): Remove all non-used user hooks
    And replace the few remaining by converting the event to hooks instead
    of the other way around.
    
    Signed-off-by: Carl Schwan <carlschwan@kde.org>

--- tests/lib/Authentication/Login/PreLoginHookCommandTest.php
+++ tests/lib/Authentication/Login/PreLoginHookCommandTest.php
@@ -10,21 +10,20 @@ declare(strict_types=1);
 namespace Test\Authentication\Login;
 
 use OC\Authentication\Login\PreLoginHookCommand;
-use OC\User\Manager;
-use OCP\IUserManager;
+use OCP\EventDispatcher\IEventDispatcher;
+use OCP\User\Events\BeforeUserLoggedInEvent;
 use PHPUnit\Framework\MockObject\MockObject;
 
 class PreLoginHookCommandTest extends ALoginTestCommand {
-	/** @var IUserManager|MockObject */
-	private $userManager;
+	private IEventDispatcher&MockObject $eventDispatcher;
 
 	protected function setUp(): void {
 		parent::setUp();
 
-		$this->userManager = $this->createMock(Manager::class);
+		$this->eventDispatcher = $this->createMock(IEventDispatcher::class);
 
 		$this->cmd = new PreLoginHookCommand(
-			$this->userManager
+			$this->eventDispatcher,
 		);
 	}
 
